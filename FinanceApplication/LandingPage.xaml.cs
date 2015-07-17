3dfe6c188e6f5d5c50df153fc2bc533f008a2812
Kevin Korir <cayvowcorir@gmail.com>
2015-07-17 12:57:27 +0300

    Completed Student View and mainpage layout

---
+++ FinanceApplication/LandingPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FinanceApplication
+{
+    /// <summary>
+    /// Interaction logic for LandingPage.xaml
+    /// </summary>
+    public partial class LandingPage : Page
+    {
+        public LandingPage()
+        {
+            InitializeComponent();
+        }
+        private void Close_Button_Click(object sender, RoutedEventArgs e)
+        {
+            App.Current.Shutdown();
+        }
+
+        private void ViewStudents_Btn_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService nav;
+            nav = NavigationService.GetNavigationService(this);
+            nav.Navigate(new Uri("StudentView.xaml", UriKind.RelativeOrAbsolute));
+        }
+
+        private void FeeStructure_Btn_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService nav;
+            nav = NavigationService.GetNavigationService(this);
+            nav.Navigate(new Uri("ModifyFeeStructure.xaml", UriKind.RelativeOrAbsolute));
+        }
+    }
+}
--- FinanceApplication/StudentPaymentRecord.xaml.cs
+++ FinanceApplication/StudentPaymentRecord.xaml.cs
@@ -12,26 +12,17 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.Windows.Navigation;
 
 namespace FinanceApplication
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for StudentPaymentRecord.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class StudentPaymentRecord : Page
     {
-        public MainWindow()
+        public StudentPaymentRecord()
         {
             InitializeComponent();
-            this.WindowState = WindowState.Maximized;
-           
         }
-
-        
-
-        
-
-       
     }
 }
